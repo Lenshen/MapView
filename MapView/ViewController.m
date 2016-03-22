bd28667fe15d021dcbeb9799605ce0eff19ac5be
Lenshen <779822644@qq.com>
2016-03-22 23:39:39 +0800

    add path for map view
    添加了两个地点的路径路线以及大头针

--- MapView/ViewController.m
+++ MapView/ViewController.m
@@ -26,7 +26,7 @@
 }
 -(void)initMapView
 {
-    self.mapView.mapType = MKMapTypeStandard;
+    self.mapView.mapType = MKMapTypeHybrid;
     self.mapView.showsUserLocation = YES;
     self.mapView.zoomEnabled = YES;
 
@@ -43,11 +43,14 @@
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
         self.locationManager.distanceFilter = kCLDistanceFilterNone;
         [self.locationManager requestAlwaysAuthorization];
+     
+
     }else
     {
         NSLog(@"error");
     }
 }
+
 -(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations
 {
     [self.locationManager stopUpdatingLocation];
@@ -62,22 +65,76 @@
     CLLocationCoordinate2D pos = [currentLocation coordinate];
 
     [self showMapViewCoordinateRegion:pos];
+    CLLocationCoordinate2D sportsAuthorityField = CLLocationCoordinate2DMake(30.747747, 120.658671);
+    [self findDirectionsFrom:currentLocation.coordinate t0:sportsAuthorityField];
     
    
 }
-//显示的范围视角和大头针功能。
+//显示的范围视角。
 -(void)showMapViewCoordinateRegion:(CLLocationCoordinate2D )locationcoordinat
 {
-    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(locationcoordinat, 500, 500);
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(locationcoordinat, 200, 200);
     
     MKCoordinateRegion region1 = [_mapView regionThatFits:region];
     [_mapView setRegion:region1 animated:YES];
     
    
 }
+-(void)findDirectionsFrom:(CLLocationCoordinate2D )sourcesCoordination
+                       t0:(CLLocationCoordinate2D )destinationCoordination
+{
+    MKPlacemark *sourcesPlaceMark = [[MKPlacemark alloc]initWithCoordinate:sourcesCoordination addressDictionary:nil];
+    MKMapItem *sourceMapItem = [[MKMapItem alloc]initWithPlacemark:sourcesPlaceMark];
+    MKPlacemark *destinationPlacemark = [[MKPlacemark alloc]initWithCoordinate:destinationCoordination addressDictionary:nil];
+    MKMapItem *destinationMapItem = [[MKMapItem alloc]initWithPlacemark:destinationPlacemark];
+    [self findDirectionsFrom:sourceMapItem to:destinationMapItem];
+}
+-(void)findDirectionsFrom:(MKMapItem *)sources to:(MKMapItem *)destination
+{
+    MKDirectionsRequest *request = [[MKDirectionsRequest alloc]init];
+    request.source = sources;
+    request.destination = destination;
+    request.requestsAlternateRoutes = NO;
+    MKDirections *directions = [[MKDirections alloc]initWithRequest:request];
+    [directions calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"error");
+        }else
+        {
+            [self showDirectionsOnMap:response];
+        }
+    }];
+    
+}
+
+-(MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay
+{
+    if ([overlay isKindOfClass:[MKPolyline class]])
+    {
+        MKPolylineRenderer *renderer = [[ MKPolylineRenderer alloc]initWithOverlay:overlay  ];
+        renderer.lineWidth = 3;
+        renderer.strokeColor = [UIColor redColor];
+        return renderer;
+    }
+    else
+    {
+        return nil;
+    }
+    
+}
+
+-(void)showDirectionsOnMap:(MKDirectionsResponse *)response
+{
+    for (MKRoute *route in response.routes) {
+        
+        [self.mapView addOverlay:route.polyline level:MKOverlayLevelAboveRoads];
+        
+    }
+    [self.mapView addAnnotation:response.source.placemark];
+    [self.mapView addAnnotation:response.destination.placemark];
+}
 -(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
 {
-    userLocation.title = @"杭州";
 [mapView setCenterCoordinate:userLocation.location.coordinate animated:YES];
 }
 - (void)didReceiveMemoryWarning {
